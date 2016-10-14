ccc2549bb0250bec472c6e823db53a34fa93b720
osterhagen <omar.nraza@gmail.com>
2016-10-13 22:26:17 -0400

    Add files via upload
    Using a bottom up tree development structure, about to switch to a top down to decrease the O()

--- ExpressionApplication.cpp
+++ ExpressionApplication.cpp
@@ -1,4 +1,5 @@
 #include "ExpressionApplication.h"
+#include <string>
 #include "ShuntingYard.h"
 
 using namespace std;
@@ -6,20 +7,66 @@ using namespace std;
 BinaryTree *
 ExpressionApplication::buildExpressionTree(vector<Token> &v) {
 	//TODO 3
-
+// copy and paste code from bottom
+    return _expression;
 }
 
 ExpressionApplication::ExpressionApplication(vector<Token> &v) {
 	//TODO 2
+    stack<Token> temp;
+    int i = 0;
+    int j = 0;
+    _expression = new BinaryTree();
+    _expression -> _root = new TreeNode(v[v.size() - 1]);
+    //_expression -> _root -> _left = new TreeNode(v[0]);
+
+    while (i < v.size()) i++;
+    while (j < i) {
+        if ( v[j].elementType == integer) {
+            temp.push(v[j]);
+        }
+        else if (v[j].elementType == identifier) {
+            temp.push(v[j]);
+        }
+        else if (v[j].elementType == operation) {
+
+            if (_expression -> _root -> _left == NULL && temp.size() > 0 && temp.size() <= 2) { // check if it is there
+                _expression->_root->_left = new TreeNode(v[j]);
+                _expression->_root->_left->_left = new TreeNode(temp.top());
+                temp.pop();
+                _expression->_root->_left->_right = new TreeNode(temp.top());
+                temp.pop();
+            }
+            else if (_expression -> _root -> _left != NULL && temp.size() > 0 && temp.size() <= 2) { // if there is already something there
+                _expression->_root->_right = new TreeNode(v[j]);
+                _expression->_root->_right->_left = new TreeNode(temp.top());
+                temp.pop();
+                _expression->_root->_right ->_right = new TreeNode(temp.top());
+                temp.pop();
+            }
+            else if (temp.size() == 1 && temp.top().elementType == operation) {
+                _expression -> _root = temp.top();
+                temp.pop();
+            }
+
+
+
+        }
+        j++;
+    }
+
 }
 
 ExpressionApplication::~ExpressionApplication() {
 	//TODO 2
-	delete _expression;
+
 }
 
+TreeNode * ExpressionApplication::differentiate(bool isPowerOp, char target) {
+	//TODO 5
+}
 
-TreeNode * differentiate(bool isPowerOp, char target) {
+BinaryTree * ExpressionApplication::differentiate(char target) {
 	//TODO 5
 }
 
